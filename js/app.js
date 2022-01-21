c5ebea33107cb6a7a33cc166b5db708b2ae8c054
Emiliano Blanco <emilianoblanco@gmail.com>
2022-01-21 09:58:23 -0300

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,189 @@
+// declaro variables
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+const boton = document.querySelector('#boton');
+
+// Para generar el rango de años de los coches que va a comercializar la empresa
+const years = document.createElement('option');
+const max = new Date().getFullYear();
+const min = max - 12; // 12 años de antiguedad como maximo
+
+// Para que me muestre del 2022 al 2011 de manera descendente
+for(let i = max; i >  min; i--) {
+    const option =  document.createElement('option');
+    option.value = i;
+    option.innerText = i;
+    document.querySelector('#year').appendChild(option);
+}
+
+// Datos para la busqueda
+const datosBusqueda = {
+    marca : '',
+    year: '',
+    color : '',
+    transmision: '',
+    minimo: '',
+    maximo:'',
+    puertas:''
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos(autos);
+});
+
+// Event Listeners para el formulario
+marca.addEventListener('input', e => {
+    datosBusqueda.marca = e.target.value;
+
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+year.addEventListener('input', e => {
+    datosBusqueda.year = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+minimo.addEventListener('input', e => {
+    datosBusqueda.minimo = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+
+maximo.addEventListener('input', e => {
+    datosBusqueda.maximo = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+
+puertas.addEventListener('input', e => {
+    datosBusqueda.puertas = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+transmision.addEventListener('input', e => {
+    datosBusqueda.transmision = e.target.value
+    // llamar a la función de filtrar Autos
+    filtrarAuto();
+});
+
+color.addEventListener('input', e => {
+    datosBusqueda.color = e.target.value
+    // llamar a la función de filtrar Autos
+    filtrarAuto();
+});
+
+function limpiarHTML() {
+    // Leer el elemento Resultado
+    const contenedor = document.querySelector('#resultado');
+
+    // limpiar los resultados anteriores
+    while(contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild);
+    }
+}
+
+function mostrarAutos(autos){
+    limpiarHTML();
+
+    // Mostrar el Resultado
+    const contenedor = document.querySelector('#resultado');
+
+    // Construir el HTML de los autos
+    autos.forEach(auto => {
+        const autoHTML = document.createElement('p');
+        autoHTML.innerHTML = `
+            <p>${auto.marca} ${auto.modelo} - ${auto.year} - ${auto.puertas} Puertas - Transmisión: ${auto.transmision} - Precio: ${auto.precio} - Color: ${auto.color}</p>
+            <img class="img-fluid" src="${auto.imagen}"/ width=800 height=450px>
+            <div class="row">
+                <div class="col">
+                    <a href="contacto.html"><button type="button" id="boton" class="btn btn-outline-danger">Me interesa</button></a>
+                </div>
+            </div>
+            
+        `;
+        contenedor.appendChild(autoHTML);
+    })
+}
+function noResultado() {
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.appendChild(document.createTextNode('Sin Resultados'));
+    document.querySelector('#resultado').appendChild(noResultado);
+}
+
+function filtrarAuto() {
+   const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+
+//    console.log(resultado);
+   if(resultado.length){
+        mostrarAutos(resultado);
+   } else {
+       noResultado();
+   }
+}
+
+
+// Aplica los filtros
+function filtrarMarca(auto) {
+    if(datosBusqueda.marca){
+        return auto.marca === datosBusqueda.marca;
+    } 
+    return auto;
+}
+function filtrarYear(auto) {
+    if(datosBusqueda.year){
+        return auto.year === datosBusqueda.year;
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    if(datosBusqueda.minimo){
+        return auto.precio >= datosBusqueda.minimo;
+    }
+    return auto;
+}
+function filtrarMaximo(auto) {
+    if(datosBusqueda.maximo){
+        return auto.precio <= datosBusqueda.maximo;
+    }
+    return auto;
+}
+function filtrarPuertas(auto) {
+    if(datosBusqueda.puertas){
+        return auto.puertas === datosBusqueda.puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    if(datosBusqueda.transmision){
+        return auto.transmision === datosBusqueda.transmision;
+    } 
+    return auto;
+}
+
+function filtrarColor(auto){
+    if(datosBusqueda.color){
+        return auto.color === datosBusqueda.color;
+    } 
+    return  auto;
+}
+
+
+boton.addEventListener('click', e => {
+    console.log("se hizo click");
+    
+});
---
+++ js/autos.js
@@ -0,0 +1,212 @@
+const autos = [
+	{
+		marca: 'BMW',
+		modelo: 'M3 ',
+		year: 2020,
+		precio: 9000000,
+		puertas: 5,
+		color: 'Azul noche',
+		transmision: 'automatico',
+		imagen: 'img/BMW_m3.png'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A4', 
+        year: 2020, 
+        precio: 9500000, 
+        puertas: 5, 
+        color: 'Gris plomo', 
+        transmision: 'automatico', 
+		imagen: 'img/AUDI_a4.png'
+    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2015,
+		precio: 8000000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'automatico',
+		imagen: 'img/MUSTANG_shelby.png'
+	},
+	{
+		marca: 'Renault',
+		modelo: 'Fluence 2.0',
+		year: 2015,
+		precio: 2000000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'manual',
+		imagen: 'img/Fluence_blanco.png'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A6', 
+        year: 2015, 
+        precio: 3500000, 
+        puertas: 5, 
+        color: 'Negro', 
+        transmision: 'automatico',
+		imagen:'img/Audi_a6_negro.png' 
+    },
+	{
+		marca: 'Citroen',
+		modelo: 'DS3',
+		year: 2015,
+		precio: 2800000,
+		puertas: 3,
+		color: 'Blanco',
+		transmision: 'manual',
+		imagen: 'img/DS3_blanco.png'
+	},
+	{
+		marca: 'BMW',
+		modelo: '530',
+		year: 2012,
+		precio: 4200000,
+		puertas: 5,
+		color: 'Azul',
+		transmision: 'automatico',
+		imagen:'img/BMW_530_azul.png'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2015,
+		precio: 4500000,
+		puertas: 5,
+		color: 'Azul',
+		transmision: 'automatico',
+		imagen:'img/MB_claseC.png'
+	},
+	{
+		marca: 'Peugeot',
+		modelo: '3008',
+		year: 2018,
+		precio: 4000000,
+		puertas: 5,
+		color: 'Rojo',
+		transmision: 'manual',
+		imagen:'img/3008_rojo.png'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Mustang', 
+        year: 2019, 
+        precio: 5600000, 
+        puertas: 3, 
+        color: 'Negro', 
+        transmision: 'manual',
+		imagen:'img/Mustang_negro.png' 
+    },
+	{
+		marca: 'Peugeot',
+		modelo: '208 GTI',
+		year: 2020,
+		precio: 4500000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'automatico',
+		imagen:'img/208_gti.png'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A3', 
+        year: 2017, 
+        precio: 3100000, 
+        puertas: 5, 
+        color: 'Negro', 
+        transmision: 'manual',
+		imagen:'img/AudiA3_negro.png' 
+    },
+	{
+		marca: 'Peugeot',
+		modelo: '2008',
+		year: 2020,
+		precio: 3400000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'manual',
+		imagen:'img/2008_blanco.png'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 3800000,
+		puertas: 3,
+		color: 'Gris',
+		transmision: 'automatico',
+		imagen:'img/MB_claseC_gris.png'
+	},
+	{
+		marca: 'BMW',
+		modelo: '523',
+		year: 2013,
+		precio: 3200000,
+		puertas: 5,
+		color: 'Negro',
+		transmision: 'automatico',
+		imagen:'img/BMW_523.png'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Focus Ghia', 
+        year: 2014, 
+        precio: 1300000, 
+        puertas: 3, 
+        color: 'Negro', 
+        transmision: 'manual',
+		imagen:'img/Focus3P.png' 
+    },
+	{
+		marca: 'Ford',
+		modelo: 'Mondeo Ghia',
+		year: 2015,
+		precio: 2800000,
+		puertas: 5,
+		color: 'Azul',
+		transmision: 'automatico',
+		imagen:'img/Mondeo_azul.png'
+	},
+	{
+		marca: 'BMW',
+		modelo: '328',
+		year: 2015,
+		precio: 3200000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'automatico',
+		imagen:'img/328_blanco.png'
+	},
+	{
+		marca: 'BMW',
+		modelo: '323 M3',
+		year: 2017,
+		precio: 4200000,
+		puertas: 3,
+		color: 'Gris',
+		transmision: 'automatico',
+		imagen:'img/323_M3.png'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase E',
+		year: 2018,
+		precio: 4000000,
+		puertas: 5,
+		color: 'Blanco',
+		transmision: 'automatico',
+		imagen:'img/MB_claseE.png'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A1', 
+        year: 2016, 
+        precio: 2800000, 
+        puertas: 5, 
+        color: 'Blanco', 
+        transmision: 'automatico',
+		imagen:'img/Audi_A1.png' 
+    }
+];
