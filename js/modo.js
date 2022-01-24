ed32cdb61c5c4294fda1233b09c70f60107aa6c6
Emiliano Blanco <emilianoblanco@gmail.com>
2022-01-24 17:30:45 -0300

    localstorage para el modo oscuro para que al actualizar no se pierda

--- js/modo.js
+++ js/modo.js
@@ -3,4 +3,19 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+
+    //LocalStorage para guardar el modo
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('modo-oscuro','true');
+    }else{
+        localStorage.setItem('modo-oscuro','false');
+    }
 })
+
+//Me fijo en que modo se encuentra actualmente
+
+if(localStorage.getItem('modo-oscuro') === 'true'){
+    document.body.classList.add('dark');
+} else {
+    document.body.classList.remove('dark');
+}
